c2d0d862913e855461990415c62ac1e6b51f5465
nox999 <serbanboanca@yahoo.com>
2024-10-16 11:29:41 +0300

    Include FPDI pentru importul anexelor PDF.

--- exemplu.php
+++ exemplu.php
@@ -1,9 +1,10 @@
 <?php
 
-  require_once('tcpdf/tcpdf.php');
+  require_once('tcpdf/tcpdf.php'); // https://github.com/tecnickcom/tcpdf
+  require_once('fpdi/src/autoload.php'); // este opțional dacă se dorește includerea anexelor în format PDF (https://github.com/Setasign/FPDI)
   require_once('xml2pdf.php');
 
-  $xmlString=file_get_contents('factura.xml');
+  $xmlString=file_get_contents('exemplu.xml');
   $factura=xml2pdfParse($xmlString);
 
   if ($factura===false) {
