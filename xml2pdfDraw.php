e95ee407cba831e6562005da119baed19a390802
nox999 <serbanboanca@yahoo.com>
2024-10-23 14:00:28 +0300

    Cleanup.

--- xml2pdfDraw.php
+++ xml2pdfDraw.php
@@ -1,5 +1,13 @@
 <?php
 
+  /*
+    Copyright (C) 2024
+
+    This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
+
   function xml2pdfDrawInit($pdf,&$vars) { // inițializează funcțiile de desenare
     if (isset($pdf) && gettype($pdf)==='object' && method_exists($pdf,'SetAutoPageBreak')) {
       $pdf->SetAutoPageBreak(false);
@@ -259,10 +267,5 @@
       xml2pdfDraw($t,$pdf,$vars);
     }
   }
-
-  // xml2pdfDrawInit($pdf,$vars);
-  //
-  // echo xml2pdfDrawEval('-{{_page}}*5+6/2-5*4.3-1',$vars).'<br>';
-  // echo -2*5+6/2-5*4.3-1;
 
 ?>
