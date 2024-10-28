35fc6b54bb22dda1801f094ac957972b01859041
nox999 <serbanboanca@yahoo.com>
2024-10-28 17:35:44 +0200

    Mutat în examples/.

--- examples/exemplu.php
+++ examples/exemplu.php
@@ -2,7 +2,7 @@
 
   require_once('tcpdf/tcpdf.php'); // https://github.com/tecnickcom/tcpdf
   require_once('fpdi/src/autoload.php'); // este opțional dacă se dorește includerea anexelor în format PDF (https://github.com/Setasign/FPDI)
-  require_once('xml2pdf.php');
+  require_once('../src/xml2pdf.php');
 
   $xmlString=file_get_contents('exemplu.xml');
   $factura=xml2pdfParse($xmlString);
