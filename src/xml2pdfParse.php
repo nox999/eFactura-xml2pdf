fc6e607ef3ed867f380b1b8b0c8417bae3d3320c
nox999 <serbanboanca@yahoo.com>
2025-08-18 12:48:08 +0300

    Implementare notă de creditare.

--- src/xml2pdfParse.php
+++ src/xml2pdfParse.php
@@ -66,27 +66,35 @@
     $xml=simplexml_load_string($xmlString);
 
     if ($xml!==false) {
+      $tipDocument=false;
       if (count($xml->xpath('/Invoice'))) {
-        $dataFactura=$xml->xpath('/Invoice/IssueDate');
+        $tipDocument='Invoice';
+      } elseif (count($xml->xpath('/CreditNote'))) {
+        $tipDocument='CreditNote';
+      }
+
+      if ($tipDocument!==false) {
+        $dataFactura=$xml->xpath('/'.$tipDocument.'/IssueDate');
         if (count($dataFactura)) {
           $dataFactura=(string)$dataFactura[0][0];
           if (preg_match('/^[0-9]{4}\-[0-9]{2}-[0-9]{2}$/',$dataFactura)) {
             list($y,$m,$d)=explode('-',$dataFactura);
-            $numar=(string)$xml->xpath('/Invoice/ID')[0][0];
+            $numar=(string)$xml->xpath('/'.$tipDocument.'/ID')[0][0];
 
             $dateCompanie=xml2pdfParseID(array(
-              count($xml->xpath('/Invoice/AccountingCustomerParty/Party/PartyIdentification/ID'))?(string)$xml->xpath('/Invoice/AccountingCustomerParty/Party/PartyIdentification/ID')[0][0]:'',
-              count($xml->xpath('/Invoice/AccountingCustomerParty/Party/PartyLegalEntity/CompanyID'))?(string)$xml->xpath('/Invoice/AccountingCustomerParty/Party/PartyLegalEntity/CompanyID')[0][0]:'',
-              count($xml->xpath('/Invoice/AccountingCustomerParty/Party/PartyTaxScheme/CompanyID'))?(string)$xml->xpath('/Invoice/AccountingCustomerParty/Party/PartyTaxScheme/CompanyID')[0][0]:'',
+              count($xml->xpath('/'.$tipDocument.'/AccountingCustomerParty/Party/PartyIdentification/ID'))?(string)$xml->xpath('/'.$tipDocument.'/AccountingCustomerParty/Party/PartyIdentification/ID')[0][0]:'',
+              count($xml->xpath('/'.$tipDocument.'/AccountingCustomerParty/Party/PartyLegalEntity/CompanyID'))?(string)$xml->xpath('/'.$tipDocument.'/AccountingCustomerParty/Party/PartyLegalEntity/CompanyID')[0][0]:'',
+              count($xml->xpath('/'.$tipDocument.'/AccountingCustomerParty/Party/PartyTaxScheme/CompanyID'))?(string)$xml->xpath('/'.$tipDocument.'/AccountingCustomerParty/Party/PartyTaxScheme/CompanyID')[0][0]:'',
             ));
 
             $factura=array(
-              'nume'=>trim((string)$xml->xpath('/Invoice/AccountingCustomerParty/Party/PartyLegalEntity/RegistrationName')[0][0]),
+              'tipDocument'=>$tipDocument==='Invoice'?'Factura':'Nota de creditare',
+              'nume'=>trim((string)$xml->xpath('/'.$tipDocument.'/AccountingCustomerParty/Party/PartyLegalEntity/RegistrationName')[0][0]),
               'nrRegCom'=>trim($dateCompanie['nrRegCom']),
               'CIF'=>trim($dateCompanie['CIF']),
-              'adresa'=>trim((string)$xml->xpath('/Invoice/AccountingCustomerParty/Party/PostalAddress/StreetName')[0][0]),
-              'localitate'=>trim(preg_replace('/(SECTOR)([0-9]{1})/','Sector \\2',(string)$xml->xpath('/Invoice/AccountingCustomerParty/Party/PostalAddress/CityName')[0][0])),
-              'judet'=>trim($judete[preg_replace('/^(RO-)/','',(string)$xml->xpath('/Invoice/AccountingCustomerParty/Party/PostalAddress/CountrySubentity')[0][0])]),
+              'adresa'=>trim((string)$xml->xpath('/'.$tipDocument.'/AccountingCustomerParty/Party/PostalAddress/StreetName')[0][0]),
+              'localitate'=>trim(preg_replace('/(SECTOR)([0-9]{1})/','Sector \\2',(string)$xml->xpath('/'.$tipDocument.'/AccountingCustomerParty/Party/PostalAddress/CityName')[0][0])),
+              'judet'=>trim($judete[preg_replace('/^(RO-)/','',(string)$xml->xpath('/'.$tipDocument.'/AccountingCustomerParty/Party/PostalAddress/CountrySubentity')[0][0])]),
             );
 
             if (preg_match('/^[A-Z]{0,2}[0-9]+$/',$factura['nrRegCom'])) {
@@ -95,25 +103,25 @@
 
             $factura['dataFactura']=$dataFactura;
             $factura['numar']=$numar;
-            $factura['tip']=(string)$xml->xpath('/Invoice/InvoiceTypeCode')[0][0];
+            $factura['tip']=(string)$xml->xpath('/'.$tipDocument.'/'.$tipDocument.'TypeCode')[0][0];
             if (isset($tipuriDocument[$factura['tip']])) {
               $factura['tipText']=$tipuriDocument[$factura['tip']];
             }
-            $factura['instrumentPlata']=count($xml->xpath('/Invoice/PaymentMeans/PaymentMeansCode'))?(string)$xml->xpath('/Invoice/PaymentMeans/PaymentMeansCode')[0][0]:'-';
+            $factura['instrumentPlata']=count($xml->xpath('/'.$tipDocument.'/PaymentMeans/PaymentMeansCode'))?(string)$xml->xpath('/'.$tipDocument.'/PaymentMeans/PaymentMeansCode')[0][0]:'-';
             if (isset($instrumentePlata[$factura['instrumentPlata']])) {
               $factura['instrumentPlataText']=$instrumentePlata[$factura['instrumentPlata']];
             }
-            if (count($xml->xpath('/Invoice/Note'))) {
+            if (count($xml->xpath('/'.$tipDocument.'/Note'))) {
               $factura['nota']='';
-              for ($i=0; $i<count($xml->xpath('/Invoice/Note')); $i++) {
-                $factura['nota'].=($factura['nota']?"\n":'').trim((string)$xml->xpath('/Invoice/Note')[$i][0]);
+              for ($i=0; $i<count($xml->xpath('/'.$tipDocument.'/Note')); $i++) {
+                $factura['nota'].=($factura['nota']?"\n":'').trim((string)$xml->xpath('/'.$tipDocument.'/Note')[$i][0]);
               }
             }
 
             $factura['TVA']=array();
             $factura['totalTVA']=0;
             $factura['totalFaraTVA']=0;
-            $totaluri=$xml->xpath('/Invoice/TaxTotal/TaxSubtotal');
+            $totaluri=$xml->xpath('/'.$tipDocument.'/TaxTotal/TaxSubtotal');
             foreach($totaluri as $total) {
               $factura['totalTVA']+=(float)$total->TaxAmount;
               $factura['totalFaraTVA']+=(float)$total->TaxableAmount;
@@ -124,25 +132,34 @@
             }
             $factura['total']=$factura['totalTVA']+$factura['totalFaraTVA'];
 
+            if ($tipDocument==='CreditNote') { // face totalurile negative la nota de creditare (todo: verificat dacă este corect)
+              $factura['totalTVA']=-$factura['totalTVA'];
+              $factura['totalFaraTVA']=-$factura['totalFaraTVA'];
+              $factura['total']=-$factura['total'];
+              array_walk_recursive($factura['TVA'],function(&$v,$k) {
+                $v=-$v;
+              });
+            }
+
             $dateCompanie=xml2pdfParseID(array(
-              count($xml->xpath('/Invoice/AccountingSupplierParty/Party/PartyIdentification/ID'))?(string)$xml->xpath('/Invoice/AccountingSupplierParty/Party/PartyIdentification/ID')[0][0]:'',
-              count($xml->xpath('/Invoice/AccountingSupplierParty/Party/PartyLegalEntity/CompanyID'))?(string)$xml->xpath('/Invoice/AccountingSupplierParty/Party/PartyLegalEntity/CompanyID')[0][0]:'',
-              count($xml->xpath('/Invoice/AccountingSupplierParty/Party/PartyLegalEntity/CompanyLegalForm'))?(string)$xml->xpath('/Invoice/AccountingSupplierParty/Party/PartyLegalEntity/CompanyLegalForm')[0][0]:'',
-              count($xml->xpath('/Invoice/AccountingSupplierParty/Party/PartyTaxScheme/CompanyID'))?(string)$xml->xpath('/Invoice/AccountingSupplierParty/Party/PartyTaxScheme/CompanyID')[0][0]:'',
+              count($xml->xpath('/'.$tipDocument.'/AccountingSupplierParty/Party/PartyIdentification/ID'))?(string)$xml->xpath('/'.$tipDocument.'/AccountingSupplierParty/Party/PartyIdentification/ID')[0][0]:'',
+              count($xml->xpath('/'.$tipDocument.'/AccountingSupplierParty/Party/PartyLegalEntity/CompanyID'))?(string)$xml->xpath('/'.$tipDocument.'/AccountingSupplierParty/Party/PartyLegalEntity/CompanyID')[0][0]:'',
+              count($xml->xpath('/'.$tipDocument.'/AccountingSupplierParty/Party/PartyLegalEntity/CompanyLegalForm'))?(string)$xml->xpath('/'.$tipDocument.'/AccountingSupplierParty/Party/PartyLegalEntity/CompanyLegalForm')[0][0]:'',
+              count($xml->xpath('/'.$tipDocument.'/AccountingSupplierParty/Party/PartyTaxScheme/CompanyID'))?(string)$xml->xpath('/'.$tipDocument.'/AccountingSupplierParty/Party/PartyTaxScheme/CompanyID')[0][0]:'',
             ));
 
-            $factura['firmaNume']=(string)$xml->xpath('/Invoice/AccountingSupplierParty/Party/PartyLegalEntity/RegistrationName')[0][0];
+            $factura['firmaNume']=(string)$xml->xpath('/'.$tipDocument.'/AccountingSupplierParty/Party/PartyLegalEntity/RegistrationName')[0][0];
             $factura['firmaNrRegCom']=$dateCompanie['nrRegCom'];
             $factura['firmaCIF']=$dateCompanie['CIF'];
-            $factura['firmaAdresa']=(string)$xml->xpath('/Invoice/AccountingSupplierParty/Party/PostalAddress/StreetName')[0][0];
-            $factura['firmaLocalitate']=preg_replace('/(SECTOR)([0-9]{1})/','Sector \\2',(string)$xml->xpath('/Invoice/AccountingSupplierParty/Party/PostalAddress/CityName')[0][0]);
-            $factura['firmaJudet']=$judete[preg_replace('/^(RO-)/','',(string)$xml->xpath('/Invoice/AccountingSupplierParty/Party/PostalAddress/CountrySubentity')[0][0])];
-            $factura['firmaCont']=count($xml->xpath('/Invoice/PaymentMeans/PayeeFinancialAccount/ID'))?(string)$xml->xpath('/Invoice/PaymentMeans/PayeeFinancialAccount/ID')[0][0]:'';
-            $factura['firmaContNume']=count($xml->xpath('/Invoice/PaymentMeans/PayeeFinancialAccount/Name'))?(string)$xml->xpath('/Invoice/PaymentMeans/PayeeFinancialAccount/Name')[0][0]:'';
-            $factura['firmaBanca']=count($xml->xpath('/Invoice/PaymentMeans/PayeeFinancialAccount/FinancialInstitutionBranch/ID'))?(string)$xml->xpath('/Invoice/PaymentMeans/PayeeFinancialAccount/FinancialInstitutionBranch/ID')[0][0]:'';
+            $factura['firmaAdresa']=(string)$xml->xpath('/'.$tipDocument.'/AccountingSupplierParty/Party/PostalAddress/StreetName')[0][0];
+            $factura['firmaLocalitate']=preg_replace('/(SECTOR)([0-9]{1})/','Sector \\2',(string)$xml->xpath('/'.$tipDocument.'/AccountingSupplierParty/Party/PostalAddress/CityName')[0][0]);
+            $factura['firmaJudet']=$judete[preg_replace('/^(RO-)/','',(string)$xml->xpath('/'.$tipDocument.'/AccountingSupplierParty/Party/PostalAddress/CountrySubentity')[0][0])];
+            $factura['firmaCont']=count($xml->xpath('/'.$tipDocument.'/PaymentMeans/PayeeFinancialAccount/ID'))?(string)$xml->xpath('/'.$tipDocument.'/PaymentMeans/PayeeFinancialAccount/ID')[0][0]:'';
+            $factura['firmaContNume']=count($xml->xpath('/'.$tipDocument.'/PaymentMeans/PayeeFinancialAccount/Name'))?(string)$xml->xpath('/'.$tipDocument.'/PaymentMeans/PayeeFinancialAccount/Name')[0][0]:'';
+            $factura['firmaBanca']=count($xml->xpath('/'.$tipDocument.'/PaymentMeans/PayeeFinancialAccount/FinancialInstitutionBranch/ID'))?(string)$xml->xpath('/'.$tipDocument.'/PaymentMeans/PayeeFinancialAccount/FinancialInstitutionBranch/ID')[0][0]:'';
 
             $factura['produse']=array();
-            $lines=$xml->xpath('/Invoice/InvoiceLine');
+            $lines=$xml->xpath('/'.$tipDocument.'/'.$tipDocument.'Line');
             foreach($lines as $line) {
               $produs=array(
                 'produs'=>trim((string)$line->Item->Name),
@@ -151,8 +168,8 @@
                 'codVanzator'=>trim((string)$line->Item->SellersItemIdentification->ID),
                 'pretFaraTVA'=>(float)$line->Price->PriceAmount,
                 'moneda'=>(string)$line->Price->PriceAmount->attributes()->currencyID,
-                'cantitate'=>(int)$line->InvoicedQuantity,
-                'um'=>(string)$line->InvoicedQuantity->attributes()->unitCode,
+                'cantitate'=>$tipDocument==='Invoice'?(int)$line->InvoicedQuantity:-(int)$line->CreditedQuantity, // face cantitățile negative la nota de creditare (todo: verificat dacă este corect)
+                'um'=>$tipDocument==='Invoice'?(string)$line->InvoicedQuantity->attributes()->unitCode:(string)$line->CreditedQuantity->attributes()->unitCode,
                 'TVA'=>(float)$line->Item->ClassifiedTaxCategory->Percent,
                 'totalFaraTVA'=>(float)$line->LineExtensionAmount,
               );
@@ -162,43 +179,45 @@
               $factura['produse'][]=$produs;
             }
 
-            if (count($xml->xpath('/Invoice/AllowanceCharge'))) { // AllowanceCharge la nivel de document (nu face la nivel de produs, todo?)
-              foreach($xml->xpath('/Invoice/AllowanceCharge') as $ac) {
-                $acProdus='';
-                if (count($ac->xpath('AllowanceChargeReasonCode')) && $acTipuri[(string)$ac->xpath('AllowanceChargeReasonCode')[0]]) {
-                  $acProdus=$acTipuri[(string)$ac->xpath('AllowanceChargeReasonCode')[0]];
-                }
-
-                $acDescriere='';
-                if (count($ac->xpath('AllowanceChargeReason'))) {
-                  $acDescriere=trim((string)$ac->xpath('AllowanceChargeReason')[0]);
-                  if (count($ac->xpath('AllowanceChargeReasonCode')) && trim((string)$ac->xpath('AllowanceChargeReasonCode')[0])) {
-                    $acDescriere.=' (Cod: '.trim((string)$ac->xpath('AllowanceChargeReasonCode')[0]).')';
+            if ($tipDocument==='Invoice') { // ignoră AllowanceCharge la nota de creditare (todo: verificat dacă este corect)
+              if (count($xml->xpath('/'.$tipDocument.'/AllowanceCharge'))) { // AllowanceCharge la nivel de document (nu face la nivel de produs, todo?)
+                foreach($xml->xpath('/'.$tipDocument.'/AllowanceCharge') as $ac) {
+                  $acProdus='';
+                  if (count($ac->xpath('AllowanceChargeReasonCode')) && $acTipuri[(string)$ac->xpath('AllowanceChargeReasonCode')[0]]) {
+                    $acProdus=$acTipuri[(string)$ac->xpath('AllowanceChargeReasonCode')[0]];
                   }
-                }
 
-                $produs=array(
-                  'produs'=>$acProdus,
-                  'descriere'=>$acDescriere,
-                  'nota'=>'',
-                  'codVanzator'=>'',
-                  'pretFaraTVA'=>(float)$ac->xpath('Amount')[0],
-                  'moneda'=>(string)$ac->xpath('Amount')[0]->attributes()->currencyID,
-                  'cantitate'=>1,
-                  'um'=>'buc.',
-                  'TVA'=>(float)$ac->xpath('TaxCategory/Percent')[0],
-                  'totalFaraTVA'=>(float)$ac->xpath('Amount')[0],
-                );
-                if ((string)$ac->xpath('ChargeIndicator')[0]==='false') { // Este Allowance?
-                  $produs['pretFaraTVA']=-$produs['pretFaraTVA'];
-                  $produs['totalFaraTVA']=-$produs['totalFaraTVA'];
+                  $acDescriere='';
+                  if (count($ac->xpath('AllowanceChargeReason'))) {
+                    $acDescriere=trim((string)$ac->xpath('AllowanceChargeReason')[0]);
+                    if (count($ac->xpath('AllowanceChargeReasonCode')) && trim((string)$ac->xpath('AllowanceChargeReasonCode')[0])) {
+                      $acDescriere.=' (Cod: '.trim((string)$ac->xpath('AllowanceChargeReasonCode')[0]).')';
+                    }
+                  }
+
+                  $produs=array(
+                    'produs'=>$acProdus,
+                    'descriere'=>$acDescriere,
+                    'nota'=>'',
+                    'codVanzator'=>'',
+                    'pretFaraTVA'=>(float)$ac->xpath('Amount')[0],
+                    'moneda'=>(string)$ac->xpath('Amount')[0]->attributes()->currencyID,
+                    'cantitate'=>1,
+                    'um'=>'buc.',
+                    'TVA'=>(float)$ac->xpath('TaxCategory/Percent')[0],
+                    'totalFaraTVA'=>(float)$ac->xpath('Amount')[0],
+                  );
+                  if ((string)$ac->xpath('ChargeIndicator')[0]==='false') { // Este Allowance?
+                    $produs['pretFaraTVA']=-$produs['pretFaraTVA'];
+                    $produs['totalFaraTVA']=-$produs['totalFaraTVA'];
+                  }
+                  $factura['produse'][]=$produs;
                 }
-                $factura['produse'][]=$produs;
               }
             }
 
             $factura['fisiere']=array();
-            $files=$xml->xpath('/Invoice/AdditionalDocumentReference/Attachment/EmbeddedDocumentBinaryObject');
+            $files=$xml->xpath('/'.$tipDocument.'/AdditionalDocumentReference/Attachment/EmbeddedDocumentBinaryObject');
             if (count($files)) {
               foreach($files as $file) {
                 if ((string)$file[0]['mimeCode']==='application/pdf') {
@@ -217,7 +236,7 @@
           return 3;
         }
       } else {
-        // Error: Not invoice
+        // Error: Unsupported document
         return 2;
       }
     } else {
